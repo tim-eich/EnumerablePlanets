8af231171ad3e9846a0f2e061ff86e6ee3bf9356
Tim-E <caffeinated.ink@gmail.com>
2024-05-28 21:06:28 -0400

    Matches example from text, but not working

---
+++ src/main/java/org/launchcode/models/Planets.java
@@ -0,0 +1,23 @@
+package org.launchcode.models;
+
+public enum Planets {
+
+    MERCURY("Mercury"),
+    VENUS("Venus"),
+    EARTH("Earth"),
+    MARS("Mars"),
+    JUPITER("Jupiter"),
+    SATURN("Saturn"),
+    URANUS("Uranus"),
+    NEPTUNE("Neptune");
+
+    private final String name;
+
+    Planets(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
